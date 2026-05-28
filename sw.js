b0e8e3aeb98fa8b9139f0648bb988f1c1adebf26
SDA Music Realm <kats.km90@gmail.com>
2026-05-28 15:46:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // HymnDesk Control · Service Worker
 // ============================================================================
 
-const CACHE_VERSION = 'hdctl-v0.27.0';
+const CACHE_VERSION = 'hdctl-v0.28.0';
 const CORE_ASSETS = [
   '/',
   '/index.html',
